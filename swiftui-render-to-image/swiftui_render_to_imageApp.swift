a159321575148037ccbed59dbae3f35ef50cee47
Stéphane Bressani <stephane@bressani.dev>
2022-09-03 18:51:32 +0200

    Initial Commit

---
+++ swiftui-render-to-image/swiftui_render_to_imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_render_to_imageApp.swift
+//  swiftui-render-to-image
+//
+//  Created by Stéphane on 03.09.22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_render_to_imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
